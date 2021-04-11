05988ed840c5719961d40df923caf68d265730d3
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2021-04-11 18:21:31 +0900

    エラー修正

--- libs/gtag.js
+++ libs/gtag.js
@@ -1,7 +1,4 @@
-export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-
-// IDが取得できない場合を想定する
-export const existsGaId = GA_ID !== "";
+export const GA_ID = "G-099R6F7XYL";
 
 // PVを測定する
 export const pageview = (path) => {
