7769cdf597f8caa86bceda0bc9e444179a6b4556
kamy18112 <kamahori.dev@gmail.com>
2022-10-30 23:54:19 +0900

    お知らせ追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,18 +95,18 @@ const Index = () => {
           <dd>
             <ul>
               <li>
+                <span>2022.10.30</span>
+                <a href="/news/news_221030.pdf" target="_blank">
+                  【新メニュー】男の髪のダメージ補修に『メンズ トリートメント』
+                </a>
+              </li>
+              <li>
                 <span>2022.10.28</span>
                 <span>【11月の店休日】11/23 (水・祝)</span>
               </li>
               <li>
                 <span>2022.09.21</span>
                 <Link href="/recruit/">【求人情報】スタイリスト、アシスタント 大至急募集！</Link>
-              </li>
-              <li>
-                <span>2022.02.22</span>
-                <a href="/news/news_220222.pdf" target="_blank">
-                  【新メニュー】ビジネスマンのための『男の眉毛 整え方実践レクチャー』
-                </a>
               </li>
               {/* <li> */}
               {/*    <span>2022.01.11</span> */}
