d3c926b42428e6297a15c5848636c3c4a753bd7a
kamy18112 <kamahori.dev@gmail.com>
2022-12-18 19:48:05 +0900

    求人ページ　コンテンツ更新

--- pages/recruit/index.tsx
+++ pages/recruit/index.tsx
@@ -46,19 +46,16 @@ const Recruit = () => (
         <section>
           <h1 className={styles.heading}>スタッフ募集</h1>
           <div className={styles.headerCopy}>
-            <p>開店 1 周年のメンズ特化美容室</p>
-            <p className={styles.headerCopyItem}>ナンバー2 として、一緒に店を大きくしてくれるスタイリスト</p>
-            <p className={styles.headerCopyItem}>髪型に悩めるメンズの救世主となってくれるスタイリスト</p>
+            <p className={styles.headerCopyItem}>この求人を見ていただいている「あなた！」のセンスと、 <br />スキルを必要としているお客様が待っています！</p>
+            <p>&quot;オシャレしたいけれど、どうしていいか分からない&quot;<br />
+              お客様のお悩みを解決する「救世主」となってくれる<br />
+              メンズ特化のスタイリスト、美容師を募集しています！</p>
           </div>
         </section>
         <div className={styles.message}>
           <img src="/img/bee_recruit.jpg" alt="松井オーナー" />
           <p>
-            オーナーの松井です。Bee dandy はオープンから1年たち、おかげさまで多くのお客様にご利用いただいております。
-            一方で、予約がとりにくい状況となってしまい、お客様にご不便をおかけしている状況になっています。そこで、
-            <span>Bee dandy のナンバー2 として、一緒に店を大きくしてくれるスタイリストさん</span>
-            <span>髪型に悩めるメンズの救世主となってくれるスタイリストさん</span>
-            を大至急、募集しております！
+            オーナーの松井です。<br />2021年にオープンしてから順調に顧客数を伸ばしています。今後はメンズだけでなく、レディースも含めて10店舗の展開を目指しています。地方から東京のサロンへ挑戦したいと思っている方も、ぜひチャレンジしてほしいです！
           </p>
         </div>
         <div className={styles.ctaBoxWrapper}>
@@ -66,42 +63,38 @@ const Recruit = () => (
         </div>
         <div className={styles.section}>
           <h2 className={styles.subHeading}>詳細</h2>
-          <dl className={styles.detail}>
-            <dt>Bee dandyのナンバー2として、一緒に店を大きくしてくれるスタイリストさん</dt>
-            <dd>
-              お客様の施術はもちろん、集客やマーケティング、経営のことにも携わっていただきます。
-              お店の運営のため、日々の受付業務、予約管理、薬剤や店販の在庫管理なども手伝っていただきます。
-              いま詳しくなくても大丈夫です。私と一緒にやりながら、スキルやノウハウを身につけていきましょう。
-            </dd>
-            <dt>悩めるメンズの＜救世主＞となってくれるスタイリストさん</dt>
-            <dd>
-              Bee dandy のお客様は９割がビジネスマンです。
-              「どういう髪型が自分に合っているのか分からない」「白髪や薄毛が気になってきたが、相談するのは恥ずかしい」
-              お客様の悩みを解決して、「かっこよくしてあげたい」と思える方、ぜひ仲間になってください！
-            </dd>
-          </dl>
+
           <section className={styles.description}>
-            <h3>Bee dandy があなたにできること</h3>
-            <dl className={styles.descriptionInner}>
-              <dt>・ あなたのファンをたくさん作ってください</dt>
-              <dd>原則として、1 人のお客様を最初から最後まで担当していただきます。</dd>
-              <dt>・ 雇用形態は不問、まずはご相談ください</dt>
-              <dd>正社員／契約社員／パート・アルバイト／業務委託／W ワーク／フリーランス</dd>
-              <dt>・ 月 1 回の日曜定休日を活用して、プライベートも充実させてください</dt>
-              <dd>連休取得も可能です。休憩時間もちゃんととっていただきます。</dd>
-            </dl>
+            <h3>指名料は100％還元、お客様満足がアナタの稼ぎに！</h3>
+            <div className={styles.descriptionContent}>
+              <div>
+                <p>
+                  完全シフト希望、シェアサロンとのWワーク、安心の保障制度など相談しながらご自身に合った契約内容で働けます。
+                </p>
+                <ul className={styles.checkedList}>
+                  <li>完全シフト、完全歩合で好きな日数と好きな休日 Wワークも可</li>
+                  <li>日当 10,000 円保証(実働８H)</li>
+                  <li>指名料は 100％還元</li>
+                </ul>
+              </div>
+            </div>
           </section>
 
           <section className={styles.description}>
-            <h3>Bee dandy が目指すこと</h3>
-            <div className={styles.descriptionContent}>
-              <p>
-                これから 10 年間で 10 店舗を展開したい会社です。 いま勢いのあるメンズ美容から展開を始め、
-                順調に顧客数を伸ばしています。 今後は、メンズだけでなく、レディースの展開も視野に入れています。
-                流行の最先端である東京と、私の故郷でもある福岡から、美容と健康、そしてトレンドを発信できる会社にしたい。
-                そんな私と一緒に、お店を、そして会社を大きくしてくれる方を募集しています！
-              </p>
-            </div>
+            <h3>給与の一例</h3>
+            <dl className={styles.descriptionInner}>
+              <dt>スタイリストの場合</dt>
+              <dd>日給10,000円×25日勤務の場合＝250,000円（勤務時間応相談）</dd>
+              <dd className={styles.withIcon}>
+                <span className={styles.icon}>＋歩合</span>（指名料1,000円×80名）の場合＝80,000円
+              </dd>
+              <dd className={styles.withIcon}>
+                <span className={styles.icon}>＋技売</span>：50万～10％ 100万～20％
+              </dd>
+              <dd className={styles.withIcon}>
+                <span className={styles.icon}>＋店販</span>：10％
+              </dd>
+            </dl>
             <div className={styles.btn}>
               <CtaButton
                 text="応募する・見学する・問い合わせする"
@@ -111,21 +104,41 @@ const Recruit = () => (
           </section>
 
           <section className={styles.description}>
-            <h3>アナタに合わせた集客プラン、アナタのスキル次第でしっかり稼げる</h3>
+            <h3>自分のための自由な時間も確保できます！</h3>
+            <div className={styles.descriptionContent}>
+              <div>
+                <p>
+                  働く環境をしっかり考えています！ Bee dandy は高単価な顧客をがっちりつかんでいくサロンです。
+                </p>
+                <p>
+                  正社員の場合、働く時間は実質1日8時間程度。休憩時間をちゃんと確保します。 月1回、日曜定休日があります。連休取得も可能です。プライベートも充実させてください。
+                </p>
+                <p>
+                  ビジネススキルも身につけたいという方には、集客ノウハウやマーケティング、経営なども学べます。いつかお店を持ちたい！という方にも満足いただける環境です。
+                </p>
+                <div className={styles.btn}>
+                  <CtaButton
+                    text="応募する・見学する・問い合わせする"
+                    link="https://work.salonboard.com/slnH000530403/input/"
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className={styles.description}>
+            <h3>Bee dandy があなたにできること</h3>
             <div className={styles.descriptionContent}>
               <img src="/img/owner_cut_2.jpg" alt="お客様と談笑しながらのカット風景" />
               <div className={styles.descriptionTextArea}>
-                <p>
-                  頑張った分はきちんと評価し、お客様満足がアナタの稼ぎに。すでにキャパフルのため、新規顧客を優先して
-                  対応していただきます。完全シフト希望、シェアサロンとのWワーク、 安心の保障制度など相談しながら
-                  ご自身に合った契約内容で働けます。
-                </p>
-                <p>例えば</p>
-                <ul className={styles.checkedList}>
-                  <li>完全シフト、完全歩合で好きな日数と好きな休日 Wワークも可</li>
-                  <li>日当 10,000 円保証(実働８H)</li>
-                  <li>指名料は 100％還元</li>
-                </ul>
+                <dl className={styles.descriptionInner}>
+                  <dt>・ あなたのファンをたくさん作ってください</dt>
+                  <dd>Bee dandyは高単価な顧客をがっちりつかんでいくサロンです。原則として、1人のお客様を最初から最後まで担当していただきます。</dd>
+                  <dt>・ 安定した新規集客、月40人から80人</dt>
+                  <dd>入社してもカットする機会がないとか、集客や営業のノルマを押し付けるようなことはありません。</dd>
+                  <dt>・ 雇用形態は不問、まずはご相談ください</dt>
+                  <dd>正社員／契約社員／パート・アルバイト／業務委託／W ワーク／フリーランス</dd>
+                </dl>
               </div>
             </div>
             <div className={styles.btn}>
@@ -137,18 +150,15 @@ const Recruit = () => (
           </section>
 
           <section className={styles.description}>
-            <h3>誇りを持てる仕事をしながら、自分のための自由な時間も確保できる</h3>
+            <h3>Bee dandy が目指すこと</h3>
             <div className={styles.descriptionContent}>
               <img src="/img/owner_cut_1.jpg" alt="真剣な表情でカットを行うオーナー" />
               <div className={styles.descriptionTextArea}>
-                <p>働く環境をしっかり考えています！ Bee dandy は高単価な顧客をがっちりつかんでいくサロンです。</p>
                 <p>
-                  正社員の場合、働く時間は実質1日8時間程度。休憩時間をちゃんと確保します。
-                  月1回、日曜定休日があります。連休取得も可能です。プライベートも充実させてください。
+                  これから 10 年間で 10 店舗を展開したい会社です。
                 </p>
-                <p>
-                  ビジネススキルも身につけたいという方には、集客ノウハウやマーケティング、経営なども学べます。いつかお店を持ちたい！という方にも満足いただける環境です。
-                </p>
+                <p>いま勢いのあるメンズ美容から展開を始め、順調に顧客数を伸ばしています。今後は、メンズだけでなく、レディースの展開も視野に入れています。</p>
+                <p>流行の最先端である東京と、私の故郷でもある福岡から、美容と健康、そしてトレンドを発信できる会社にしたい。そんな私と一緒に、お店を、そして会社を大きくしてくれる方を募集しています！</p>
               </div>
             </div>
             <div className={styles.btn}>
@@ -177,49 +187,21 @@ const Recruit = () => (
           </section>
 
           <section className={styles.description}>
-            <h3>給与の一例</h3>
-            <dl className={styles.descriptionInner}>
-              <dt>スタイリストの場合</dt>
-              <dd>日給10,000円×25日勤務の場合＝250,000円（勤務時間応相談）</dd>
-              <dd className={styles.withIcon}>
-                <span className={styles.icon}>＋歩合</span>（指名料1,000円×80名）の場合＝80,000円
-              </dd>
-              <dd className={styles.withIcon}>
-                <span className={styles.icon}>＋技売</span>：50万～10％ 100万～20％
-              </dd>
-              <dd className={styles.withIcon}>
-                <span className={styles.icon}>＋店販</span>：10％
-              </dd>
-            </dl>
-          </section>
-
-          <section className={styles.description}>
-            <h3>メッセージ：松井祐太（オーナー）</h3>
+            <h3>スタッフの声</h3>
+            <p className={styles.voice}>『求められるレベルは高いですが！こんなに楽しく、ストレスなく働けて、稼げる環境がある事を知ってほしいです！！』 （スタイリスト：深沢）</p>
             <div className={styles.descriptionContent}>
-              <img className={styles.spNone} src="/img/owner_full.jpg" alt="松井オーナー" />
-              <img className={styles.pcNone} src="/img/owner_full_pc.jpg" alt="松井オーナー" />
+              <img src="/img/bee_hukazawa.jpg" alt="笑みを浮かべるスタイリスト深沢" />
               <div className={styles.descriptionTextArea}>
                 <p>
-                  僕と一緒に、Bee dandy を大きくしてくれませんか！
-                  たくさんの感動と自分への誇りを得られる美容室を作りたい。そんな想いで福岡から上京し、2021年、ビジネスマンの街、新橋に
-                  Bee dandy をオープンしました。
-                  月1回、日曜定休日があります。連休取得も可能です。プライベートも充実させてください。
+                  原宿のお店に入った後、フリーランスになったのですが、経営知識を学びたいのと、オーナーの人柄に魅かれてBee dandyに入社しました。
                 </p>
                 <p>
-                  この求人を見ていただいている「あなた」のセンスと、スキルを必要としているお客様が待っています！
-                  オシャレしたいけど、どうしていいか分からない、そんなお客様のお悩みを解決できる、救世主となってくれる、メンズ特化のスタイリスト、美容師を募集しています。
+                  オーナーは何より人が大好きで、人間味に溢れています。だからこそ、お客様へ提供する「価値」にもこだわりがあるので、サービスや商材がしっかりしています。その分、客単価が高く、客層もちゃんとした方が多いというのはすごく理解できます。
                 </p>
                 <p>
-                  今後は店舗や新しいサービスの展開も考えています。地方から東京のサロンへ挑戦したいと思っている方がいらっしゃったら、できるだけサポートします。私も共感できるところが
-                  あるので、ぜひチャレンジしてほしい！そんなあなたを待っています！
+                  ぶっちゃけ、収入が倍になりました。しかもストレス無し(笑)。美容師ってずっと働いても稼げない・・・と感じている方も多いと思います。しかし、Bee dandyに入社して、そうではない世界がここにあるのを知り、視野がグンと広がりました。
                 </p>
               </div>
-            </div>
-            <div className={styles.btn}>
-              <CtaButton
-                text="応募する・見学する・問い合わせする"
-                link="https://work.salonboard.com/slnH000530403/input/"
-              />
             </div>
           </section>
 
