a7a0ca0a7de67014146cd3cc8451d4dbc742b255
kamy18112 <kamahori.dev@gmail.com>
2022-12-18 18:54:44 +0900

    お知らせ更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,6 +95,10 @@ const Index = () => {
           <dd>
             <ul>
               <li>
+                <span>2022.12.18</span>
+                <span>【年末年始休暇】12/30 (金) ~ 1/4 (水)</span>
+              </li>
+              <li>
                 <span>2022.12.05</span>
                 <span>【12月の店休日】12/11 (日)</span>
               </li>
@@ -102,13 +106,6 @@ const Index = () => {
                 <span>2022.11.27</span>
                 <a href='https://youtu.be/OAH_y70Xwuw' target="_blank" rel="noreferrer">【ニュース】-Kim Channel-【湘南美容クリニック】で松井が紹介されました</a>
               </li>
-              <li>
-                <span>2022.10.30</span>
-                <a href="/news/news_221030.pdf" target="_blank">
-                  【新メニュー】男の髪のダメージ補修に『メンズ トリートメント』
-                </a>
-              </li>
-
               {/* <li> */}
               {/*   <span>2022.09.21</span> */}
               {/*   <Link href="/recruit/">【求人情報】スタイリスト、アシスタント 大至急募集！</Link> */}
