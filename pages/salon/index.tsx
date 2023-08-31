68d1fd436b30dd020f49ba67ad62beb44b9a5c24
kamy18112 <kamahori.dev@gmail.com>
2023-09-01 02:36:15 +0900

    新型コロナウィルス感染症の感染拡大の防止についてを削除する

--- pages/salon/index.tsx
+++ pages/salon/index.tsx
@@ -62,40 +62,6 @@ const Salon = () => (
             <p className={styles.message_name}>オーナー 松井祐太</p>
           </section>
 
-          <section className={styles.info}>
-            <h2 className={styles.subHeading}>新型コロナウィルス感染症の感染拡大の防止について</h2>
-            <dl className={styles.info_contents}>
-              <div>
-                <dt>当社の取り組み</dt>
-                <dd>
-                  <ul className={styles.info_list}>
-                    <li>スタッフは手洗い、手指消毒を徹底しております。また、マスクを着用して対応させていただきます</li>
-                    <li>随時、消毒液にて店内の清掃をしております。</li>
-                    <li>定期的に店内の換気をしております。</li>
-                    <li>
-                      スタッフの体温が37.5℃以上で風邪のような症状が出ている場合、出勤停止となりご予約の変更をお願いする場合がございます。
-                    </li>
-                  </ul>
-                </dd>
-              </div>
-              <div>
-                <dt>お客様へのお願い</dt>
-                <dd>
-                  <ul className={styles.info_list}>
-                    <li>
-                      ご来店いただいたお客様全員に、次亜塩素酸水またはアルコールでの手の消毒をお願いしております。
-                    </li>
-                    <li>
-                      以下の場合、ご来店をお断りする場合がございますのでご了承ください。
-                      <br />
-                      体温が37.5℃以上ある・咳が出ている | 1ヶ月以内に海外渡航歴がある | 身近にコロナウイルス感染者がいる
-                    </li>
-                  </ul>
-                </dd>
-              </div>
-            </dl>
-            <p className={styles.info_notice}>※皆様のご理解、ご協力をよろしくお願い致します。</p>
-          </section>
         </section>
       </div>
     </MainLayout>
