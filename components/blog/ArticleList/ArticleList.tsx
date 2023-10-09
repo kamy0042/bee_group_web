10349f77e9b008b5d1113f801253ad191af062d3
kamy18112 <kamahori.dev@gmail.com>
2023-10-10 03:39:10 +0900

    feat: 一覧画面スタイル調整

--- components/blog/ArticleList/ArticleList.tsx
+++ components/blog/ArticleList/ArticleList.tsx
@@ -1,5 +1,6 @@
 import { ArticleListItem } from "../ArticleListItem/ArticleListItem";
 import { Article } from "../../../libs/microcms";
+import styles from "./index.module.scss"
 
 type Props = {
   articles?: Article[];
@@ -13,7 +14,7 @@ export default function ArticleList({ articles }: Props) {
     return <p>記事がありません。</p>;
   }
   return (
-    <ul>
+    <ul className={styles.list}>
       {articles.map((article) => (
         <ArticleListItem key={article.id} article={article} />
       ))}
