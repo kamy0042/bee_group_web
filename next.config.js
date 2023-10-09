976557a88c46e2ca5eceaa677ba0e6f511ba450c
kamy18112 <kamahori.dev@gmail.com>
2023-10-10 08:13:14 +0900

    refactor: exportPathMapを取り除く

--- next.config.js
+++ next.config.js
@@ -1,30 +1,6 @@
-module.exports = {
-  exportPathMap() {
-    return {
-      "/": {
-        page: "/",
-      },
-      "/menu": {
-        page: "/menu",
-      },
-      "/salon": {
-        page: "/salon",
-      },
-      "/staff": {
-        page: "/staff",
-      },
-      "/faq": {
-        page: "/faq",
-      },
-      "/recruit": {
-        page: "/recruit",
-      },
-      "/voice": {
-        page: "/voice",
-      },
-      "/waxing": {
-        page: "/waxing",
-      },
-    };
-  },
-};
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
+
