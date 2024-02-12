a6bad40b20f11f4ff9a22c8f81f6f673d5fe65f1
kamy18112 <kamahori.dev@gmail.com>
2024-02-12 22:37:19 +0900

    feat: pagination追加

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 import { getList } from "../../libs/microcms";
 import ArticleList from "../../components/blog/ArticleList/ArticleList";
 import styles from "../../styles/Waxing.module.scss";
+import Pagination from "../../components/blog/Pagination";
+import { LIMIT } from "../../constants";
 
 
 export const revalidate = 60;
@@ -13,12 +15,13 @@ export async function generateMetadata(): Promise<any> {
 }
 
 export default async function Page (){
-  const list = await getList();
+  const list = await getList({limit:LIMIT});
 
   return (
       <div>
         <h2 className={styles.heading}>Blog</h2>
         <ArticleList articles={list.contents} />
+        <Pagination totalCount={list.totalCount}  />
       </div>
   );
 } 
